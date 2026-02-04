27af014be66cbe0dc06360bb4ce740e7450083d9
Pj Salita <pj.salita@gmail.com>
2026-02-04 18:50:23 +0800

    Add GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { AppDock } from "@/components/app-dock";
 import { Profile } from "@/components/home/profile";
 import { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "PJ Salita - Passionate Full-Stack Web Developer",
@@ -17,6 +18,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=UA-145746351-1`} />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-145746351-1', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </head>
       <body className="antialiased">
         <div className="bg-background text-foreground relative min-h-screen w-full overflow-hidden lg:flex lg:h-screen">
           <Profile />
